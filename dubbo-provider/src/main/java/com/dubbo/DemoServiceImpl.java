93756b3ec206c334a05956cb336506770c4432d8
wangwencheng@szy.cn <wangwencheng@szy.cn>
2018-05-17 11:38:20 +0800

    dubbotest

---
+++ dubbo-provider/src/main/java/com/dubbo/DemoServiceImpl.java
@@ -0,0 +1,15 @@
+package com.dubbo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoServiceImpl implements DemoService {
+    @Override
+    public List<String> getPermissions(Long id) {
+        List<String> demo = new ArrayList<String>();
+        demo.add(String.format("Permission_%d", id - 1));
+        demo.add(String.format("Permission_%d", id));
+        demo.add(String.format("Permission_%d", id + 1));
+        return demo;
+    }
+}
